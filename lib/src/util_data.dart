377399fc8754f38f395e262b80df88957a6acdf2
robmllze <86869208+robmllze@users.noreply.github.com>
2022-11-17 17:51:05 +1100

    Update util_data.dart

--- lib/src/util_data.dart
+++ lib/src/util_data.dart
@@ -31,7 +31,7 @@ Set<T>? maybeAddToSet<T>(Set<T>? source, Set<T>? add) {
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-T mergeLists<T extends Iterable>(T a, dynamic b) {
+T mergeListsOrSets<T extends Iterable>(T a, dynamic b) {
   final aa = a is List
       ? List.of(a)
       : a is Set
@@ -50,6 +50,34 @@ T mergeLists<T extends Iterable>(T a, dynamic b) {
 
 // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
+Iterable mergeIterables(dynamic a, dynamic b) {
+  final aa = a is Iterable ? a: [a];
+  final bb = b is Iterable ? b: [b];
+  return aa.followedBy(bb);
+}
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+T mergeListsOrSets<T extends Iterable>(T a, dynamic b) {
+  final aa = a is List
+      ? List.of(a)
+      : a is Set
+          ? Set.of(a)
+          : null;
+  final bb = b is List
+      ? List.of(b)
+      : b is Set
+          ? Set.of(b)
+          : null;
+  if (aa == null) {
+    // TODO: Describe this error.
+    throw Error();
+  }
+  return bb == null ? ((aa as dynamic)..add(b)) : ((aa as dynamic)..addAll(bb));
+}
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
 dynamic mergeDataDeep(
   dynamic a,
   dynamic b, [
@@ -60,19 +88,19 @@ dynamic mergeDataDeep(
       a,
       b,
       value: (final a, final b) {
-        if (a is Map && b is Map) {
-          return mergeDataDeep(a, b, elseFilter);
-        }
-        if (a is List || a is Set) {
-          return mergeLists<Iterable>(a as Iterable, b);
-        }
+        if (a is Map && b is Map) return mergeDataDeep(a, b, elseFilter);
+        if (a is List || a is Set) return mergeListsOrSets<Iterable>(a as Iterable, b);
+//         if (a is List) return mergeIterables(a, b).toList();
+//         if (a is Set) return mergeIterables(a, b).toSet();
+//         if (a is Iterable) return mergeIterables(a, b);
         return elseFilter?.call(b) ?? b;
       },
     );
-  } // else
-  if (a is List || a is Set) {
-    return mergeLists<Iterable>(a as Iterable, b);
-  } // else
+  }
+  if (a is List || a is Set) return mergeListsOrSets<Iterable>(a as Iterable, b);
+//   if (a is List) return mergeIterables(a, b).toList();
+//   if (a is Set) return mergeIterables(a, b).toSet();
+//   if (a is Iterable) return mergeIterables(a, b);
   return elseFilter?.call(b) ?? b;
 }
 
