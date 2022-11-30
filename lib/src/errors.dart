a60285c29da326a90757fdde6c5f8f581133c6a0
Robert Mollentze <robmllze@gmail.com>
2022-11-30 12:14:26 +1100

    update

---
+++ lib/src/errors.dart
@@ -0,0 +1,34 @@
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// Medikinect
+//
+// Copyright Ⓒ 2022-2023, N Sanchez Medical Research Corporation
+// See LICENSE.txt for more information
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+class _XyzError {
+  final XyzErrorType type;
+  final dynamic e;
+  const _XyzError(this.e, this.type);
+  @override
+  String toString() {
+    return e.toString();
+  }
+}
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+class ProgrammerError extends _XyzError {
+  const ProgrammerError(dynamic e)
+      : super(
+          "Programmer error: $e",
+          XyzErrorType.PROGRAMMER_ERROR,
+        );
+}
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+enum XyzErrorType {
+  PROGRAMMER_ERROR,
+}
