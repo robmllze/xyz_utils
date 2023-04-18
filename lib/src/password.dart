90ce0ae00a14dae6a60ab26a50e9074aeca35981
Robert Mollentze <robmllze@gmail.com>
2023-04-18 18:27:05 -0400

    update

---
+++ lib/src/password.dart
@@ -0,0 +1,20 @@
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// XYZ Utils
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+import 'dart:math' show Random;
+
+String generateDeterministicPassword(
+  List<Object> source,
+  int seed, [
+  int range = 256,
+]) {
+  final encodedList = source.map((final element) {
+    return Random(element.hashCode).nextInt(range);
+  }).toList();
+  final decodedList = encodedList.map((final code) => (code - seed) % range).toList();
+  final password = String.fromCharCodes(decodedList);
+  return password;
+}
