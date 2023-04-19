0d5df7a35d0bcbf0be967ec3f49da2114736612f
Robert Mollentze <robmllze@gmail.com>
2023-04-18 20:29:04 -0400

    update

--- lib/src/password.dart
+++ lib/src/password.dart
@@ -12,10 +12,25 @@ String generateDeterministicPassword(
   int seed, [
   int range = 256,
 ]) {
-  final encodedList = garbage.map((final element) {
-    return Random(element.hashCode).nextInt(range);
-  }).toList();
-  final decodedList = encodedList.map((final code) => (code * seed) % range).toList();
-  final password = String.fromCharCodes(decodedList);
+  final length = garbage.length;
+
+  // Check that the length of the garbage list is greater than 1.
+  assert(length > 1);
+  final half = length ~/ 2;
+
+  // Choose a random range for the subset of garbage to use for the password.
+  final range = Random(seed).nextInt(half) + half; // half >= range <= garbage length
+  final subset = garbage.sublist(0, range);
+
+  // Map the subset to hashCodes.
+  final hashCodes = subset.map((final element) => element.hashCode).toList();
+
+  // Encode the hashCodes using a deterministic formula.
+  final encoded = hashCodes
+      .map((final code) => (Random(seed).nextInt(range) * code * seed * length) % range)
+      .toList();
+
+  // Convert the character codes to a string to obtain the final password.
+  final password = String.fromCharCodes(encoded);
   return password;
 }
