2bed025a4beb74f0e02489180a6f362970af6b28
Robert Mollentze <robmllze@gmail.com>
2023-08-29 17:04:07 +1200

    update

---
+++ lib/src/non_web/fix_dart_file.dart
@@ -0,0 +1,22 @@
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// XYZ Utils
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+import 'dart:io';
+
+import '../UNSORTED.dart';
+import '../app/here.dart';
+
+// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+
+/// Applies fixes to the dart file at [filePath].
+Future<void> fixDartFile(String filePath) async {
+  try {
+    final fixedPath = getFixedPath(filePath);
+    await Process.run("dart", ["fix", "--apply", fixedPath]);
+  } catch (e) {
+    Here().debugLogError(e);
+  }
+}
--- lib/src/non_web/non_web.dart
+++ lib/src/non_web/non_web.dart
@@ -5,10 +5,11 @@
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
 export 'analyze_annotated_classes.dart';
+export 'fix_dart_file.dart';
+export 'fmt_dart_file.dart';
+export 'generate.dart';
 export 'io/file_system.dart';
 export 'io/list_file_paths.dart';
 export 'io/list_folder_paths.dart';
-export 'fmt_dart_file.dart';
-export 'generate.dart';
 
 export 'debug/download_and_extract_zip.dart';
