7bd5654ede36302b0fec0a6a19611b135cebda9c
Robert Mollentze <robmllze@gmail.com>
2023-08-29 13:17:33 +1200

    update

--- lib/src/non_web/debug/download_and_extract_zip.dart
+++ lib/src/non_web/debug/download_and_extract_zip.dart
@@ -4,15 +4,15 @@
 //
 // ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
 
-import 'package:archive/archive_io.dart'; // debug dependency
-import 'dart:io';
+// import 'package:archive/archive_io.dart'; // debug dependency
+// import 'dart:io';
 
-// ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+// // ░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
 
-Future<void> downloadAndExtractZip(String url, String destinationFolder) async {
-  final request = await HttpClient().getUrl(Uri.parse(url));
-  final response = await request.close();
-  final bytes = await response.fold<List<int>>([], (final a, final b) => a..addAll(b));
-  var archive = ZipDecoder().decodeBytes(bytes);
-  extractArchiveToDisk(archive, destinationFolder);
-}
+// Future<void> downloadAndExtractZip(String url, String destinationFolder) async {
+//   final request = await HttpClient().getUrl(Uri.parse(url));
+//   final response = await request.close();
+//   final bytes = await response.fold<List<int>>([], (final a, final b) => a..addAll(b));
+//   var archive = ZipDecoder().decodeBytes(bytes);
+//   extractArchiveToDisk(archive, destinationFolder);
+// }
