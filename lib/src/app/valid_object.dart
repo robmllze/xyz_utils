e53e8274238d2bbba2e61071a4ba5e91d065bddd
Robert Mollentze <robmllze@gmail.com>
2023-08-31 08:06:17 +1200

    update

---
+++ lib/src/app/valid_object.dart
@@ -0,0 +1,23 @@
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+//
+// XYZ Utils
+//
+// ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+
+abstract class ValidObject {
+  const ValidObject();
+  bool get valid;
+  static bool areValid(List<dynamic> inputs) {
+    for (final input in inputs) {
+      if (input == null) {
+        return false;
+      }
+      try {
+        if (input.isEmpty) {
+          return false;
+        }
+      } catch (_) {}
+    }
+    return true;
+  }
+}
